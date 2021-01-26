c84699190a8743904b87409b1712475c9f558243
John Zhen <john.zhen24@myhunter.cuny.edu>
2021-01-25 22:21:52 -0500

    refactored code

--- main.py
+++ main.py
@@ -19,27 +19,51 @@ root.geometry(dimension)
 
 executable_path = "c:\se\chromedriver.exe"
 
+
 def getUsername():
     username = usernameEntry.get()
     return username
 
+
 def getPassword():
     password = passwordEntry.get()
-    return password;
+    return password
+
 
 def fillInCredentials(usernameField, passwordField, user, pw):
     usernameField.send_keys(user)
     passwordField.send_keys(pw)
 
-def go():
-    driver = webdriver.Chrome(executable_path)
-    bblink = "https://ssologin.cuny.edu/cuny.html?resource_url=https%3A%2F%2Fbbhosted.cuny.edu%252F"
-    driver.get(bblink)
+
+def gotoBB(driver):
+    bburl = "https://ssologin.cuny.edu/cuny.html?resource_url=https%3A%2F%2Fbbhosted.cuny.edu%252F"
+    driver.get(bburl)
+
+
+def fillOutForm(driver):
+    driver.switch_to.parent_frame()
     user = driver.find_element_by_id("CUNYfirstUsernameH")
     pw = driver.find_element_by_id("CUNYfirstPassword")
     theirUsername = getUsername()
     theirPassword = getPassword()
-    fillInCredentials(user,pw, theirUsername, theirPassword)
+    fillInCredentials(user, pw, theirUsername, theirPassword)
+    submitButton = driver.find_element_by_id("submit")
+    submitButton.click()
+
+def handleRedirect(driver):
+    driver.switch_to.parent_frame()
+    bblink = driver.find_element_by_xpath("/html/body/div[1]/div/div/ul/li[1]/a")
+    bblink.click()
+
+
+def go():
+    driver = webdriver.Chrome(executable_path)
+    gotoBB(driver)
+    fillOutForm(driver)
+    handleRedirect(driver)
+    fillOutForm(driver)
+    driver.switch_to.parent_frame()
+    print(driver.title)
 
 
 getButton = tk.Button(root, text="Go", command=go).grid(row=2, column=1)
